d57e9c477d7ae2bd75b82506fac6fd428e0afd23
cjc7373 <niuchangcun@gmail.com>
2021-01-22 11:26:29 +0800

    revoke atomic requests
    seems this will cause Database is locked error, because call unified login consumes lots of time

--- course_assessment/settings.py
+++ course_assessment/settings.py
@@ -68,7 +68,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'ATOMIC_REQUESTS': True,
         'OPTIONS': {
             'timeout': 10,
         },
