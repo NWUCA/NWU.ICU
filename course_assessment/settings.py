d8b080d09154362de72b0d0936f82f1aece0a358
cjc7373 <niuchangcun@gmail.com>
2021-01-22 10:02:52 +0800

    try to solve Database is locked error

--- course_assessment/settings.py
+++ course_assessment/settings.py
@@ -68,6 +68,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'ATOMIC_REQUESTS': True,
+        'OPTIONS': {
+            'timeout': 2,
+        },
     }
 }
 
