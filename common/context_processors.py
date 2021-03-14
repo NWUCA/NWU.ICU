1813f5c3dcd703571b5d3591ff92d1d3e870f409
cjc7373 <niuchangcun@gmail.com>
2021-03-14 15:42:55 +0800

    feat: add version

--- common/context_processors.py
+++ common/context_processors.py
@@ -1,5 +1,12 @@
+import subprocess
+
 from common.models import Announcement
 
 
 def announcements(request):
     return {'announcements': Announcement.objects.filter(enabled=True)}
+
+
+def version(request):
+    res = subprocess.run(["git", "describe", "--tag"], capture_output=True)
+    return {'version': res.stdout.decode().strip()}
