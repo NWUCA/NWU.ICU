2f05790417e211429bf7fcee8e395ea9ed95ab1d
cjc7373 <niuchangcun@gmail.com>
2021-04-19 20:17:53 +0800

    use django form to create review

--- course_assessment/admin.py
+++ course_assessment/admin.py
@@ -5,4 +5,8 @@ from course_assessment.models import Course, Review, School, Teacher
 admin.site.register(School)
 admin.site.register(Course)
 admin.site.register(Teacher)
-admin.site.register(Review)
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'course', 'content', 'rating', 'created_by', 'anonymous')
