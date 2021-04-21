e93665a10ec2856e37d6f4af0b4973f47d3438af
cjc7373 <niuchangcun@gmail.com>
2021-04-21 09:35:48 +0800

    fix ordering

--- course_assessment/admin.py
+++ course_assessment/admin.py
@@ -3,10 +3,14 @@ from django.contrib import admin
 from course_assessment.models import Course, Review, School, Teacher
 
 admin.site.register(School)
-admin.site.register(Course)
 admin.site.register(Teacher)
 
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ('id', 'course', 'content', 'rating', 'created_by', 'anonymous')
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'classification', 'teacher', 'school', 'created_by')
