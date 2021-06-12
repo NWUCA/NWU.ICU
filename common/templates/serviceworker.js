15027ca078f81d763bfb71cdb9672553bb3f7868
cjc7373 <niuchangcun@gmail.com>
2021-06-12 16:44:32 +0800

    Optimize the navigation page

--- common/templates/serviceworker.js
+++ common/templates/serviceworker.js
@@ -1,42 +1,67 @@
 // {% load static %}
 
-const cacheName = 'NWU.ICU-v1';
-const appShellFiles = [
-    'https://unpkg.com/vue@3.0.9/dist/vue.global.js',
+const VERSION = '{{ version }}';
+
+const CACHE_NAME = `NWU.ICU-${VERSION}`;
+const appFiles = [
+    '/', // 缓存首页使首页能够秒开
 ]
 
+const log = function () {
+    console.log.call(console, '[Service Worker]', ...arguments)
+}
+
 self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
+    console.log('[Service Worker] Installing SW version: ', VERSION);
     e.waitUntil((async () => {
-        const cache = await caches.open(cacheName);
-        console.log('[Service Worker] Caching all: app shell and content');
-        await cache.addAll(appShellFiles);
+        const cache = await caches.open(CACHE_NAME);
+        log('Caching', appFiles)
+        await cache.addAll(appFiles);
     })());
 });
 
 self.addEventListener('activate', event => {
     event.waitUntil((async () => {
-        return true;
+        // delete old caches
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        log('deleting', cacheName)
+                        return caches.delete(cacheName)
+                    }
+                })
+            )
+        })
     })());
 });
 
+const cacheRequest = async (request) => {
+    const response = await fetch(request);
+    const cache = await caches.open(CACHE_NAME);
+    await cache.put(request, response.clone());
+    return response
+}
+
 self.addEventListener('fetch', (e) => {
-    console.log(e.request)
-    if (e.request.url.startsWith('chrome-extension') || e.request.method !== 'GET' || e.request.destination === 'document') {
-        console.log(`[Service Worker] Skipping dealing with resource: ${e.request.url}`);
+    console.log('[Service Worker] Receiving fetch request: ', e.request)
+    // FIXME: 有无更好的处理 '/' 路径的方法?
+    if (e.request.url === 'http://127.0.0.1:8000/' || e.request.url === 'https://nwu.icu/') {}
+    else if (e.request.url.startsWith('chrome-extension') || e.request.method !== 'GET' || e.request.destination === 'document') {
+        console.log(`[Service Worker] Skipping: ${e.request.url}`);
         return
     }
     e.respondWith((async () => {
         const r = await caches.match(e.request);
-        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
         if (r) {
-            console.log(`[Service Worker] Using cached resource: ${e.request.url}`);
+            console.log(`[Service Worker] Cache hit: ${e.request.url}`);
+            if (r.url === 'http://127.0.0.1:8000/' || r.url === 'https://nwu.icu/') {
+                log('Re-caching "/"')
+                cacheRequest(e.request)
+            }
             return r;
         }
-        const response = await fetch(e.request);
-        const cache = await caches.open(cacheName);
-        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-        await cache.put(e.request, response.clone());
-        return response;
+        console.log(`[Service Worker] Cache miss, fetching: ${e.request.url}`);
+        return await cacheRequest(e.request);
     })());
 });
