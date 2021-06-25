fb3e7565cdaf1a0300558555acaa9ad64441c3fc
cjc7373 <niuchangcun@gmail.com>
2021-06-25 13:45:57 +0800

    refactor: use generic view to list courses

--- course_assessment/views.py
+++ course_assessment/views.py
@@ -4,30 +4,33 @@ from django.db import IntegrityError
 from django.db.models import Avg, Q
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views import View
+from django.views.generic import ListView
 
 from course_assessment.models import Course, Review
 
 from .models import CourseForm, ReviewForm, TeacherForm
 
 
-class CourseList(View):
-    def get(self, request):
-        search_string = request.GET.get('s', "")
+class CourseList(ListView):
+    template_name = 'course_list.html'
+    model = Course
+
+    def get_queryset(self):
+        search_string = self.request.GET.get('s', "")
+        course_set = self.model.objects.all()
         if search_string:
-            course_set = Course.objects.filter(
+            course_set = self.model.objects.filter(
                 Q(name__contains=search_string) | Q(teacher__name__contains=search_string)
             )
-        else:
-            course_set = Course.objects.all()
-        context = {
-            'courses': course_set.order_by('school').annotate(rating=Avg('review__rating')),
-            'search_string': search_string,
-        }
-        return render(request, 'course_list.html', context=context)
+        return course_set
 
-    def post(self, request):
-        messages.success(request, 'a test message')
-        return redirect('/')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['courses'] = (
+            self.get_queryset().order_by('school').annotate(rating=Avg('review__rating'))
+        )
+        context['search_string'] = self.request.GET.get('s', "")
+        return context
 
 
 class TeacherView(LoginRequiredMixin, View):
