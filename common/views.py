171ff4f1a13dac4accdd78837230566ee1fb405b
cjc7373 <niuchangcun@gmail.com>
2021-08-13 18:39:31 +0800

    feat: web push can now be used for test

--- common/views.py
+++ common/views.py
@@ -1,8 +1,12 @@
 import json
 
-from django.contrib.auth.decorators import login_required
+from django.conf import settings as dj_settings
+from django.contrib.auth.decorators import login_required, permission_required
 from django.http import HttpResponse
 from django.shortcuts import render
+from pywebpush import webpush
+
+from .models import WebPushSubscription
 
 
 def index(request):
@@ -15,12 +19,35 @@ def about(request):
 
 @login_required
 def settings(request):
-    return render(request, 'settings.html')
+    context = {"VAPID_PUBLIC_KEY": dj_settings.WEBPUSH_SETTINGS["VAPID_PUBLIC_KEY"]}
+    return render(request, 'settings.html', context=context)
 
 
+@login_required
 def save_push_subscription(request):
-    print(json.loads(request.body))
+    data = json.loads(request.body)
+    WebPushSubscription.objects.update_or_create(
+        user=request.user, defaults={'subscription': data}
+    )
     return HttpResponse()
+
+
+@permission_required('dumb_perm')  # superuser has all the permissions
+def send_test_notification(request):
+    try:
+        msg = request.GET['msg']
+        for s in WebPushSubscription.objects.all():
+            webpush(
+                s.subscription,
+                msg,
+                vapid_private_key=dj_settings.WEBPUSH_SETTINGS['VAPID_PRIVATE_KEY'],
+                vapid_claims={
+                    "sub": f"mailto:{dj_settings.WEBPUSH_SETTINGS['VAPID_ADMIN_EMAIL']}"
+                },
+            )
+        return HttpResponse('OK')
+    except KeyError:
+        return HttpResponse(status=400)
 
 
 def manifest(request):
