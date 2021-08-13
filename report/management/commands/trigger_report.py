e83e23e9a55c44dc26b9d49e51058cae7ac4fed4
MooWantFree <46401523+MooWantFree@users.noreply.github.com>
2021-08-13 22:08:05 +0800

    Update trigger_report.py
    Optimized hints

--- report/management/commands/trigger_report.py
+++ report/management/commands/trigger_report.py
@@ -26,11 +26,20 @@ class Command(BaseCommand):
         with futures.ThreadPoolExecutor(workers) as executor:
             results = executor.map(self.do_report, report_list)
         success = 0
-        for result in results:
+        veri = 0
+        net_error = 0
+        for (result, status) in results:
             if result:
                 success += 1
+            elif status == 'veri':
+                veri += 1
+            elif status == 'net_error':
+                net_error += 1
         logger.critical(
-            f'成功人数: {success}/{len(report_list)}, 用时: {time.time() - start_time:.2f}s'
+            f'成功人数: {success}/{len(report_list)}, 开启二级验证人数{veri}, 网络错误人数{net_error}, '
+            f'人数差={len(report_list) - veri - net_error}=成功人数'
+            f'{success==len(report_list) - veri - net_error},'
+            f' 用时: {time.time() - start_time:.2f}s'
         )
 
     def model_save_with_retry(self, report: Report):
@@ -97,12 +106,12 @@ class Command(BaseCommand):
                     if report.last_report_message:
                         report.last_report_message = ''
                         self.model_save_with_retry(report)
-                    return True
+                    return (True, 'success')
                 else:
                     logger.warning(f'{report.user.username}-{report.user.name} {r}')
                     report.last_report_message = f'[{datetime.now()} {r}]'
                     self.model_save_with_retry(report)
-                    return False
+                    return (False, 'veri')
             except (requests.exceptions.ConnectionError, requests.exceptions.SSLError) as e:
                 i = i + 1
                 sleep(1)
@@ -114,4 +123,4 @@ class Command(BaseCommand):
                     )
                     report.last_report_message = f'[{datetime.now()} 连接失败\n' f'错误信息: {e}]'
                     self.model_save_with_retry(report)
-                    return False
+                    return (False, 'net_error')
