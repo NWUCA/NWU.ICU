2d28f44626a6d141602a8b2552ec8f8d7d357bf7
cjc7373 <niuchangcun@gmail.com>
2021-08-23 19:47:18 +0800

    serviceworker: disable cache for now

--- common/templates/serviceworker.js
+++ common/templates/serviceworker.js
@@ -13,11 +13,11 @@ const log = function () {
 
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Installing SW version: ', VERSION);
-    e.waitUntil((async () => {
-        const cache = await caches.open(CACHE_NAME);
-        log('Caching', appFiles)
-        await cache.addAll(appFiles);
-    })());
+    // e.waitUntil((async () => {
+    //     const cache = await caches.open(CACHE_NAME);
+    //     log('Caching', appFiles)
+    //     await cache.addAll(appFiles);
+    // })());
 });
 
 self.addEventListener('activate', event => {
@@ -43,28 +43,28 @@ const cacheRequest = async (request) => {
     return response
 }
 
-self.addEventListener('fetch', (e) => {
-    console.log('[Service Worker] Receiving fetch request: ', e.request)
-    // FIXME: 有无更好的处理 '/' 路径的方法?
-    if (e.request.url === 'http://127.0.0.1:8000/' || e.request.url === 'https://nwu.icu/') {}
-    else if (e.request.url.startsWith('chrome-extension') || e.request.method !== 'GET' || e.request.destination === 'document') {
-        console.log(`[Service Worker] Skipping: ${e.request.url}`);
-        return
-    }
-    e.respondWith((async () => {
-        const r = await caches.match(e.request);
-        if (r) {
-            console.log(`[Service Worker] Cache hit: ${e.request.url}`);
-            if (r.url === 'http://127.0.0.1:8000/' || r.url === 'https://nwu.icu/') {
-                log('Re-caching "/"')
-                cacheRequest(e.request)
-            }
-            return r;
-        }
-        console.log(`[Service Worker] Cache miss, fetching: ${e.request.url}`);
-        return await cacheRequest(e.request);
-    })());
-});
+// self.addEventListener('fetch', (e) => {
+//     console.log('[Service Worker] Receiving fetch request: ', e.request)
+//     // FIXME: 有无更好的处理 '/' 路径的方法?
+//     if (e.request.url === 'http://127.0.0.1:8000/' || e.request.url === 'https://nwu.icu/') {}
+//     else if (e.request.url.startsWith('chrome-extension') || e.request.method !== 'GET' || e.request.destination === 'document') {
+//         console.log(`[Service Worker] Skipping: ${e.request.url}`);
+//         return
+//     }
+//     e.respondWith((async () => {
+//         const r = await caches.match(e.request);
+//         if (r) {
+//             console.log(`[Service Worker] Cache hit: ${e.request.url}`);
+//             if (r.url === 'http://127.0.0.1:8000/' || r.url === 'https://nwu.icu/') {
+//                 log('Re-caching "/"')
+//                 cacheRequest(e.request)
+//             }
+//             return r;
+//         }
+//         console.log(`[Service Worker] Cache miss, fetching: ${e.request.url}`);
+//         return await cacheRequest(e.request);
+//     })());
+// });
 
 self.addEventListener('push', function(event) {
     console.log(event)
