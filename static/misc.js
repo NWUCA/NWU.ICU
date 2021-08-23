a409a00e5d25c577b0d15eaf2d740c3f6b3a2939
cjc7373 <niuchangcun@gmail.com>
2021-08-23 20:57:14 +0800

    fix: safari does not support event.submitter

--- static/misc.js
+++ static/misc.js
@@ -2,7 +2,8 @@ let forms = document.querySelectorAll("form");
 for (let form of forms) {
     form.addEventListener("submit", (event) => {
         // console.log(event)
-        let btn = event.submitter;
+        // let btn = event.submitter; // Safari does not support this feature...
+        let btn = form.querySelectorAll("button[type='submit']")[0];
         btn.disabled = true;
         btn.innerHTML = `<span class="spinner-border spinner-border-sm" role="status"
                             aria-hidden="true"></span>加载中...`;
