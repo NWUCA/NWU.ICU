5dafef5ff48aa9f5e409f3ed39faf64975f07c16
cjc7373 <niuchangcun@gmail.com>
2022-05-11 21:22:24 +0800

    feat: cache version function

--- course_assessment/admin.py
+++ course_assessment/admin.py
@@ -8,6 +8,7 @@ admin.site.register(School)
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ('id', 'course', 'content', 'rating', 'created_by', 'anonymous')
+    # 若 course 不为只读, 会产生和课程数相等的 SQL queries, 因每一课程都会去查相关联的教师表
     readonly_fields = ('course',)
 
 
