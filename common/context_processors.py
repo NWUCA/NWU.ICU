12e3b2cc7fe7881bc957fddcb285de885865adbf
cjc7373 <niuchangcun@gmail.com>
2022-05-11 21:22:24 +0800

    feat: cache version function

--- common/context_processors.py
+++ common/context_processors.py
@@ -1,4 +1,5 @@
 import subprocess
+from functools import cache
 
 from common.models import Announcement
 
@@ -8,5 +9,10 @@ def announcements(request):
 
 
 def version(request):
+    return {'version': _version()}
+
+
+@cache
+def _version() -> str:
     res = subprocess.run(["git", "describe", "--tag"], capture_output=True)
-    return {'version': res.stdout.decode().strip()}
+    return res.stdout.decode().strip()
