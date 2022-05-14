034fce91694a85db9633ecd6b698e2627e82d54f
cjc7373 <niuchangcun@gmail.com>
2022-05-14 20:11:08 +0800

    misc: UI tweaks

--- user/admin.py
+++ user/admin.py
@@ -5,10 +5,12 @@ from user.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    search_fields = ('username',)
-    list_display = ('username', 'name')
+    search_fields = ('username', 'name')
+    list_display = ('username', 'name', 'nickname')
+    list_filter = (('nickname', admin.EmptyFieldListFilter),)
     fields = (
         'username',
+        'nickname',
         'last_login',
         'is_superuser',
         'is_staff',
