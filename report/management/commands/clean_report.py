2485fd0b53ba6507ec726cc0507adb1bda7ec404
Harold Cheng <niuchangcun@gmail.com>
2022-07-23 21:22:57 +0800

    Update clean_report.py

--- report/management/commands/clean_report.py
+++ report/management/commands/clean_report.py
@@ -14,18 +14,23 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('--all', action='store_true', help='禁用所有填报')
+        parser.add_argument('-d', '--dry-run', action='store_true', help='dry run')
 
     def handle(self, *args, **options):
         if options['all']:
             reports = Report.objects.filter(status=True)
         else:
             reports = Report.objects.annotate(l=Length('user__username')).filter(
-                Q(user__username__startswith='2017') | Q(user__username__startswith='2016'),
+                Q(user__username__startswith='2017')
+                | Q(user__username__startswith='2016')
+                | Q(user__username__startswith='2018'),
                 Q(l=10) | Q(l=9),
                 status=True,
             )
         for report in reports:
             logger.info(f"正在删除 {report.user.username}-{report.user.name} 的填报")
-            report.delete()
+            if not options['dry_run']:
+                report.delete()
         logger.info("正在删除所有已关闭的填报")
-        Report.objects.filter(status=False).delete()
+        if not options['dry_run']:
+            Report.objects.filter(status=False).delete()
