24920cff3278ea8816bc277a837b5e6ed93e5a0f
Harold Cheng <niuchangcun@gmail.com>
2022-08-21 10:05:42 +0800

    login: fix captcha

--- user/forms.py
+++ user/forms.py
@@ -4,3 +4,4 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(label='用户名', max_length=20, required=True)
     password = forms.CharField(label='密码', max_length=100, required=True, widget=forms.PasswordInput)
+    captcha = forms.CharField(label='验证码', max_length=10, required=True)
