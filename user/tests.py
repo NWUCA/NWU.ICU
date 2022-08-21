a216eb0eb3c54f86a6eda33518c18bb62a1c8355
Harold Cheng <niuchangcun@gmail.com>
2022-08-21 11:31:42 +0800

    login: update tests

--- user/tests.py
+++ user/tests.py
@@ -3,19 +3,32 @@ from user.models import User
 
 
 def test_login(monkeypatch, user, client):
-    def mock_unified_login(username, raw_password):
+    def mock_unified_login(username, raw_password, captcha, captcha_cookies):
         return views.LoginResult(True, '登陆成功', user.name, b"")
 
     monkeypatch.setattr(views, 'unified_login', mock_unified_login)
 
-    r = client.post('/login/', data={'username': user.username, 'password': 'bar'})
+    # According to the doc:
+    # https://docs.djangoproject.com/en/4.1/topics/testing/tools/#django.test.Client.session
+    # To modify the session and then save it, it must be stored in a variable first
+    # (because a new SessionStore is created every time this property is accessed)
+    session = client.session
+    session['captcha_cookies'] = {"foo": "bar"}
+    session.save()
+    r = client.post('/login/', data={'username': user.username, 'password': 'bar', 'captcha': 'foo'})
+    print(r.content.decode())
     assert r.status_code == 302
-    client.logout()
+    client.logout()  # session will be deleted when logged out
 
+    session = client.session
+    session['captcha_cookies'] = {"foo": "bar"}
+    session.save()
     # login by a nonexistent user will create new user
     another = 'alice'
-    r = client.post('/login/', data={'username': another, 'password': 'bar'})
+    r = client.post('/login/', data={'username': another, 'password': 'bar', 'captcha': 'foo'})
     print(User.objects.all())
+    print(r.content.decode())
+    assert r.status_code == 302
     assert User.objects.get(username=another)
     client.logout()
 
