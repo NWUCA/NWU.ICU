bf5f1d384948849b349afa94e11777a214f98230
MooWantFree <thedark928@gmail.com>
2024-06-27 08:12:03 +0800

    修改用户数据库, 不再需要上次cookie更新时间

--- user/models.py
+++ user/models.py
@@ -5,7 +5,6 @@ from django.db import models
 class User(AbstractUser):
     name = models.TextField()
     cookie = models.BinaryField()  # A RequestsCookieJar object in requests
-    cookie_last_update = models.DateTimeField()
     nickname = models.CharField(max_length=30)
 
     REQUIRED_FIELDS = []
