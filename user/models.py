86b3e3eff255d4d15e0f3bfbb84dba931f7efe34
MooWantFree <thedark928@gmail.com>
2024-06-28 16:58:29 +0800

    更新user model

--- user/models.py
+++ user/models.py
@@ -3,11 +3,17 @@ from django.db import models
 
 
 class User(AbstractUser):
-    name = models.TextField()
-    cookie = models.BinaryField()  # A RequestsCookieJar object in requests
+    name = models.TextField(null=True)
+    password = models.CharField(max_length=255)  # A RequestsCookieJar object in requests
     nickname = models.CharField(max_length=30)
-
+    nwu_email = models.CharField(max_length=255, null=True)
     REQUIRED_FIELDS = []
 
+
+class VerificationCode(models.Model):
+    email = models.EmailField(unique=True)
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
-        return f"{self.username}-{self.name}-{self.nickname}"
+        return self.email
