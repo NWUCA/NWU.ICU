f1ba777dffb5e533998ef3ba38f2133ec70e0b5c
MooWantFree <i@moo.ac>
2024-07-14 02:48:19 +0800

    添加restful framework验证库

--- settings/settings.py
+++ settings/settings.py
@@ -55,6 +55,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
