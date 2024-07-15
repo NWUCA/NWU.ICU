360a9c57815a78680c809a03e40d1ba878a81178
MooWantFree <i@moo.ac>
2024-07-15 22:41:27 +0800

    删除弃用验证码类

--- user/models.py
+++ user/models.py
@@ -8,12 +8,3 @@ class User(AbstractUser):
     nickname = models.CharField(max_length=30)
     nwu_email = models.CharField(max_length=255, null=True)
     REQUIRED_FIELDS = []
-
-
-class VerificationCode(models.Model):
-    email = models.EmailField(unique=True)
-    code = models.CharField(max_length=6)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.email
