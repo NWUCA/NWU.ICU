815e2d9c379041f0f1291ab1dc7b178ba3464826
MooWantFree <i@moo.ac>
2024-07-16 00:07:27 +0800

    删除所有自动填报相关文件

--- report/trigger_report.sh
+++
@@ -1,4 +0,0 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR/../
-export DJANGO_SETTINGS_MODULE=settings.production  # 否则获取不到 telegram bot token..
-poetry run python manage.py trigger_report
