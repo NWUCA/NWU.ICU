91d616257d99a94abc156f7f62b376b43163dc1a
MooWantFree <i@moo.ac>
2024-07-15 22:36:19 +0800

    修改验证码最长可用时间

--- settings/settings.py
+++ settings/settings.py
@@ -182,7 +182,7 @@ CAPTCHA_LENGTH = 4
 CAPTCHA_CHALLENGE_FUNCT = 'captcha.helpers.random_char_challenge'
 CAPTCHA_NOISE_FUNCTIONS = ('captcha.helpers.noise_arcs', 'captcha.helpers.noise_dots')
 CAPTCHA_FILTER_FUNCTIONS = ('captcha.helpers.post_smooth',)
-CAPTCHA_TIMEOUT = 5 * 60  # 5 minutes
+CAPTCHA_TIMEOUT = 5  # 5 minutes
 CAPTCHA_IMAGE_BEFORE_FIELD = False
 CAPTCHA_IMAGE_SIZE = (120, 50)
 # django-silk is disabled due to lack of django 3.2 support
