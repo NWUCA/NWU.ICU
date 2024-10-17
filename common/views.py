dc4bd9463dde5e52b8341094cba8844e4b42d51b
MooWantFree <i@moo.ac>
2024-10-17 14:11:29 +0800

    chore: 去除无用依赖

--- common/views.py
+++ common/views.py
@@ -2,7 +2,6 @@ from captcha.helpers import captcha_image_url
 from captcha.models import CaptchaStore
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.db.models import Q
-from django.dispatch import receiver
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated
