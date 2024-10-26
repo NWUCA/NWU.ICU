b7b18305680a64b1aa610d73c912719e130f4fc5
MooWantFree <i@moo.ac>
2024-10-26 15:19:05 +0800

    feat:使用drf分页

---
+++ utils/custom_pagination.py
@@ -0,0 +1,17 @@
+from rest_framework.pagination import PageNumberPagination
+
+from utils.utils import return_response
+
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'pageSize'
+    max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return return_response(contents={
+            'page': self.page.number,
+            'max_page': self.page.paginator.num_pages,
+            'count': self.page.paginator.count,
+            'results': data
+        })
