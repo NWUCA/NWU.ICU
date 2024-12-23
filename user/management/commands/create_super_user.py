23f3924ba039e294f717be9669fe20d810270ecf
MooWantFree <i@moo.ac>
2024-12-23 16:31:48 +0800

    feat: 创建超级用户添加-y参数

--- user/management/commands/create_super_user.py
+++ user/management/commands/create_super_user.py
@@ -8,14 +8,21 @@ User = get_user_model()
 class Command(BaseCommand):
     help = 'Creates a default user'
 
+    def add_arguments(self, parser):
+        parser.add_argument('-y', '--yes', action='store_true', help='Automatically confirm overwriting existing user')
+
     def handle(self, *args, **options):
         super_user_id = int(settings.DEFAULT_SUPER_USER_ID)
         username = settings.DEFAULT_SUPER_USER_USERNAME
         email = settings.DEFAULT_SUPER_USER_PASSWORD
         password = settings.DEFAULT_SUPER_USER_PASSWORD
         if User.objects.filter(id=super_user_id).exists():
-            confirm = input(
-                f'User with id={super_user_id} already exists, are you sure you want to overwrite it? (Y/n): ')
+            if options['yes']:
+                self.stdout.write(self.style.WARNING(f'User with id={super_user_id} already exists, ignore operation'))
+                return
+            else:
+                confirm = input(
+                    f'User with id={super_user_id} already exists, are you sure you want to overwrite it? (Y/n): ')
             if confirm.lower() == 'n':
                 self.stdout.write(self.style.WARNING('Operation cancelled.'))
                 return
