5d5e51f939584dbd30f70bd09811ccb74586dd1e
MooWantFree <i@moo.ac>
2025-01-10 16:44:08 +0800

    chore: 更改分页大小名称

--- utils/custom_pagination.py
+++ utils/custom_pagination.py
@@ -5,7 +5,7 @@ from utils.utils import return_response
 
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 10
-    page_size_query_param = 'pageSize'
+    page_size_query_param = 'page_size'
     max_page_size = 100
 
     def get_paginated_response(self, data):
