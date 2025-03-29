e7bc34a945518780cfdcb39c72e803a3b858f3c9
MooWantFree <i@moo.ac>
2025-03-29 09:16:20 +0800

    feat: 更改分页的关键词

--- utils/custom_pagination.py
+++ utils/custom_pagination.py
@@ -5,8 +5,17 @@ from utils.utils import return_response
 
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 10
-    page_size_query_param = 'page_size'
+    page_size_query_param = 'pageSize'
     max_page_size = 100
+
+    def get_page_size(self, request):
+        page_size = request.query_params.get(self.page_size_query_param, None)
+        if page_size is None:
+            page_size = request.query_params.get('page_size', self.page_size)
+        try:
+            return min(int(page_size), self.max_page_size)
+        except (TypeError, ValueError):
+            pass
 
     def get_paginated_response(self, data):
         return return_response(contents={
