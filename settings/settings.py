113a69a6451a7420a0744c99ec4d486fec98f1f4
MooWantFree <i@moo.ac>
2025-04-15 16:24:26 +0800

    feat: static files root

--- settings/settings.py
+++ settings/settings.py
@@ -131,6 +131,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
